1c6c8b5118b7485b0b8e8d372660102f59b9aff5
Ayush Jain <ayushjain1144@gmail.com>
2017-12-24 01:06:52 +0530

    Initial commit

---
+++ Chapter 2/ex2.py
@@ -0,0 +1,7 @@
+# A comment, this is so that you can read your program later
+# anything after the # is ignored by the python
+print "I could have code like this" #after this all will be ignored
+
+# you can also use a comment to disable a line out of the code
+# print "This won't run"
+print "This will run"
